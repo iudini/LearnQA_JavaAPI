775227a43ff033275601173009cd37c7bc07dcda
i.yudin <i.yudin@sysdyn.ru>
2023-05-10 16:42:50 +0300

    parsing json

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,15 +1,19 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class HelloWorldTest {
 
     @Test
-    public void testGetText() {
-        Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/get_text")
-                .andReturn();
-        response.prettyPrint();
+    public void testRestAssured() {
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+        List<JsonPath> messages = response.get("messages");
+        System.out.println(messages.get(1));
     }
 }
